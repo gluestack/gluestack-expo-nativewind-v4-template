8db45c48977dd6cdd717b0373b4227b7ca1d1af3
Viraj-10 <virajjoshi132@gmail.com>
2024-01-25 13:11:06 +0530

    feat: added ThemeProvider and theme variables

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import "./global.css";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
-
+import { useColorScheme } from "react-native";
+import { ThemeProvider } from "@/components/ui/core/ThemeProvider";
 export default function App() {
   return (
-    <View className="flex-1 bg-white items-center justify-center">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider mode="light">
+      <View className="flex-1 bg-primary-500 items-center justify-center">
+        <Text className="text-typography-100">
+          Open up App.tsx to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
